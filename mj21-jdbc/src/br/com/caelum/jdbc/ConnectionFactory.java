f6f78dc25f15c1b017402d3e48ebba8aa9d6f6ec
Aluno <Aluno@10.174.12.117>
2024-11-05 21:39:50 -0300

    .

--- mj21-jdbc/src/br/com/caelum/jdbc/ConnectionFactory.java
+++ mj21-jdbc/src/br/com/caelum/jdbc/ConnectionFactory.java
@@ -1,5 +1,15 @@
 package br.com.caelum.jdbc;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Connection;
+
 
 public class ConnectionFactory {
+	
+	public Connection getConnection() {
+		try {
+			return DriverManager.getConnection(null, null)
+		}catch() {}
+	}
 
 }
--- mj21-jdbc/src/br/com/caelum/jdbc/TestaExercicio.java
+++ mj21-jdbc/src/br/com/caelum/jdbc/TestaExercicio.java
@@ -3,7 +3,7 @@ package br.com.caelum.jdbc;
 public class TestaExercicio {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
